44b37a3a81804daa8b2f1a0fcf10ee86cd38744d
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-12-23 02:03:50 +0900

    add: 新規登録・ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :tasks
   root "home#index"
+  resources :sessions, only: [:new]
+  resources :users, only: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
